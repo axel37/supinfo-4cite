112237280bc79b78a31a8584410a7b14414a6dbe
Axel Choteau <axelc37@outlook.fr>
2024-03-02 17:41:26 +0100

    RoomTest : testCanCreateRoom()

---
+++ api/src/Entity/Dto/RoomDto.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity\Dto;
+
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+#[ApiResource(shortName: 'Room')]
+class RoomDto
+{
+    #[NotBlank]
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
