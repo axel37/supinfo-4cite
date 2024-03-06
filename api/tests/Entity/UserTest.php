8bc8e02383c8f8f5d14a06e4ddc2106ac96d31f6
Axel Choteau <axelc37@outlook.fr>
2024-03-06 14:02:59 +0100

    User : testCanCreateBooking

--- api/tests/Entity/UserTest.php
+++ api/tests/Entity/UserTest.php
@@ -2,10 +2,13 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Hotel;
+use App\Entity\Room;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Clock\DatePoint;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class UserTest extends KernelTestCase
@@ -45,5 +48,21 @@ class UserTest extends KernelTestCase
         self::assertEquals('NewUser', $userFromDb->getUsername());
     }
 
+    public function testCanCreateBooking(): void
+    {
+        $hotel = new Hotel('Temp hotel', 'Pine street');
+        $room = new Room($hotel, 'Temp Room');
+        $this->em->persist($hotel);
+
+        $user = new User('user@example.com', 'UserName');
+        $id = $user->getId();
+        $user->setPassword('password');
+        $user->book($room, new DatePoint('today'), new DatePoint('tomorrow'));
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $userFromDb = $this->userRepository->find($id);
+        self::assertCount(1, $userFromDb->getBookings());
+    }
 
 }
